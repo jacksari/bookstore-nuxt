1d54cb923444bd6434da6b98e4fcc6e7e78828c2
Jack Sari <janasarii@gmail.com>
2021-06-28 16:11:48 -0500

    primer commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <h2>Counter {{ counter }}</h2>
+      <v-btn
+        depressed
+        color="primary"
+        @click="increment"
+      >
+        Increment
+      </v-btn>
+      <v-btn
+        depressed
+        color="error"
+        @click="decrement"
+      >
+        Decrement
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+import { mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    counter () {
+      return this.$store.state.counter
+    }
+  },
+  methods: {
+    ...mapMutations({
+      increment: 'increment',
+      decrement: 'decrement'
+    })
+  }
+}
+
+
+</script>
